b7b8e10c67720451f1b4ff22a2848969b865caaf
armaan-vala <142339466+armaan-vala@users.noreply.github.com>
2023-08-19 16:45:48 +0530

    Add files via upload

---
+++ loops1.py
@@ -0,0 +1,7 @@
+list1 = [12, -7, 5, 64, -14]
+output1 = [num for num in list1 if num >= 0]
+print(output1)  # Output: [12, 5, 64]
+
+list2 = [12, 14, -95, 3]
+output2 = [num for num in list2 if num >= 0]
+print(output2)  # Output: [12, 14, 3]
